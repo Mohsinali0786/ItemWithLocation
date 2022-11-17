9955e06e432689238239cba766907f0aa8196f38
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-11-17 16:41:22 +0500

    feat/added google Client Id

--- client/src/Components/googlelogin.jsx
+++ client/src/Components/googlelogin.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react';
 import GoogleLogin from 'react-google-login';
+import { googleClientId } from '../utils/constants'
+
 export default function MyGoogleLogin() {
-    const [logindata,setLogindata]=useState(false)
+    const [logindata, setLogindata] = useState(false)
     const responseGoogle = (response) => {
-        console.log('failure',response);
+        console.log('failure', response);
     }
-    const handleLogin=(response)=>{
-        console.log('response',response);
+    const handleLogin = (response) => {
+        console.log('response', response);
 
     }
-    const handleLogout=(response)=>{
+    const handleLogout = (response) => {
         console.log(response);
     }
     return (
         <div>
             {
-                logindata?(
+                logindata ? (
                     <div>
                         <h4>You Logged in as </h4>
-                        <button onClick={()=>{handleLogout()}}>Logout</button>
+                        <button onClick={() => { handleLogout() }}>Logout</button>
                     </div>
-    
-                )
-                :
-                ""
 
+                )
+                    :
+                    null
 
             }
             <GoogleLogin
-                clientId="948935625327-e5qssmnkteoacfjru7rq0av4sgg77sb3.apps.googleusercontent.com"
+                clientId={googleClientId}
                 buttonText="Google"
                 onSuccess={handleLogin}
                 onFailure={responseGoogle}
