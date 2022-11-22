ceb393d0296da9d8a74ac7b82ff317f15813aff4
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-11-22 16:56:56 +0500

    updated index9

--- index.js
+++ index.js
@@ -43,13 +43,7 @@ console.log('cond', cond)
 
 
 
-// app.use(express.static(path.join(__dirname, '.-client/build')))
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(".-client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  ".-client/build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, '.-client/build')))
 
 app.use(cors());
 
