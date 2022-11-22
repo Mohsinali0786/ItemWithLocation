c73c8e118f3f14ad9d070aacc5070850813a11e6
Mohsin Muhammad Ali <56477672+Mohsinali0786@users.noreply.github.com>
2022-11-22 15:59:00 +0500

    updated index3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const schedule = require('node-schedule')
 // const { mongoose } = require('./config')
 
 require('dotenv').config()
-const PORT = process.env.PORT || 4001
+const PORT = process.env.myport || 4001
 
 
 mongoose.connect(process.env.DB_URI)
